b17a2134c411866153ed9a7034e3b34bc4784e13
twikirize <twikirize@gmail.com>
2026-09-22 21:31:32 +0300

    U-01: remove duplicate ServiceUnavailableError class

--- app/utils/exceptions.py
+++ app/utils/exceptions.py
@@ -100,14 +100,3 @@ class ExternalServiceError(Exception):
         self.code = code
         self.details = details
         super().__init__(self.message)
-
-
-class ServiceUnavailableError(Exception):
-    """Raised when a service is unavailable"""
-    def __init__(self, message="Service unavailable", service_name=None, retry_after=None, code=None, details=None):
-        self.message = message
-        self.service_name = service_name
-        self.retry_after = retry_after
-        self.code = code
-        self.details = details
-        super().__init__(self.message)
